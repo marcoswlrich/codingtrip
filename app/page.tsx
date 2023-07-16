b9452a569c82171b89f96bf2efb59616be9f555e
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-16 19:54:52 -0400

    Add Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Word</h1>
-    </main>
+    <>
+      <HeroSection />
+    </>
   );
 }
