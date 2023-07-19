9504ea129184dbe16052441be425ba0da5a0c321
marcoswlrich <marcosapw.dev@gmail.com>
2023-07-19 01:36:48 -0400

    About Home component

---
+++ components/AboutHome.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "./ui/SectionTitle";
+
+export const AboutHome = () => {
+  return (
+    <section className="container py-6">
+      <SectionTitle subtitle="sobre" title="Marcos Wlrich" />
+    </section>
+  );
+};
